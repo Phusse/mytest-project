4f38147bc1cc2736ad4908db5266b2851b3d63dc
Dubem <dubemmegbo@gmail.com>
2022-11-18 00:23:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let reg = document.getElementById("input")
+let error = document.getElementById("error")
+let submit = document.getElementById("submit")
+let secError = document.getElementById("error2")
+let valid = document.getElementById("valid")
+submit.addEventListener("click", showError)
+
+function showError(){
+    event.preventDefault()
+    if (reg.value == "") {
+        error.style.display = "block"
+        secError.style.display = "none"
+        valid.style.display = "none"
+
+    }
+    else if(reg.value[0] != "R") {
+        secError.style.display = "block"
+        error.style.display = "none"
+        valid.style.display = "none"
+
+    }else{
+        error.style.display = "none"
+        secError.style.display = "none"
+        reg.value = ""
+        valid.style.display = "block"
+    }
+}
+
+
